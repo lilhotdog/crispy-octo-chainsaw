acaedc3aa246d49aaffb5674437a05f5994d1802
lilhotdog <lilmeatball33@gmail.com>
2023-11-08 20:25:10 -0500

    Signed-off-by: lilhotdog

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-from mido  import MidiFile
+from mido import MidiFile, MidiTrack, Message
+import sys
+import os
 
-mid = MidiFile('Queen - Bohemian Rhapsody.mid', clip=True)
+file = sys.argv[1] if len(sys.argv) > 1 else 'Queen - Bohemian Rhapsody.mid'
+mid = MidiFile(file, clip=True)
 new_midi = MidiFile()
 
-banned = ('drum', 'hit', 'timpani', 'perc') # Instruments we don't want
+banned_prog = (8, 9, 10, 11, 12, 13, 14, 15, 96, 97, 98, 99, 100, 101, 102, 103, 112, 113, 114, 115, 116, 118, 119) # Ban programs (FX, Percussion) ref https://jazz-soft.net/demo/GeneralMidi.html
 
+# Create a dictionary to map channels to banned instruments
+banned_channels = {}
 
-for track in mid.tracks: # Loop through all of the tracks in the midi file
-    if track[0].type == 'text':
-        track_name = track[0].text # Get the name of the track
-        not_banned = all(ban not in track_name.lower() for ban in banned) # Make sure the track isn't banned
-        if not_banned:
-            for msg in track:
-                if not msg.is_meta:
-                    msg.channel=0 # Set the midi channel to 0
-            track[0].text='Piano' # Change the track name to Piano
-            new_midi.tracks.append(track) # Add the track to the new midi
-        else:
-           print('Banished track: ', track_name) # Remove percussion
-    else:            
-        new_midi.tracks.append(track) # Add any track that's not an instrument (tempo track)
+for track in mid.tracks:
+    new_track = MidiTrack()
+    
+    for msg in track:
+        if not msg.is_meta:
+            if hasattr(msg, 'program') and msg.program in banned_prog:
+                # If a program change message is for a banned instrument, mark the channel as banned.
+                banned_channels[msg.channel] = True
+            if hasattr(msg, 'channel') and msg.channel not in banned_channels:
+                msg.channel = 0
+                # If the channel is not banned, add the message to the new track.
+                new_track.append(msg)
+    
+    new_midi.tracks.append(new_track)
 
-new_midi.save('test.mid')
+new_midi.save('new_' + os.path.basename(file))
+
+print('File saved.')
